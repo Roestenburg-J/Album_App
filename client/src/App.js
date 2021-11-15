3d8fce91a22dcc601918ed998bfbe5588ff949a3
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-11-15 09:30:08 +0200

    Added Add Photo Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+
+  const [date, setDate] = useState("");
+  const [location, setLocation] = useState("")
+  const [takenby, setTakenBy] = useState("")
+
   return (
     <div className="App">
 
-    </div>
+      <div className="photo-meta">
+        <label>Date and Time:</label>
+        <input type="text" onChange={(event) => { setDate(event.target.value); }} />
+        <label>Location:</label>
+        <input type="text" onChange={(event) => { setLocation(event.target.value); }} />
+        <label>Taken By:</label>
+        <input type="text" onChange={(event) => { setTakenBy(event.target.value); }} />
+
+        <div className="photo">
+          <form action="/">
+            <label for="img">Select image:</label>
+            <input type="file" id="img" name="img" accept="image/*"></input>
+          </form>
+        </div>
+
+        <button>Upload Photo</button>
+      </div>
+    </div >
   );
 }
 
