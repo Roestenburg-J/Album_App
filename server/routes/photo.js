f29dd38010ec32b8366794824c9ec1c853b9fad7
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-11-16 15:58:10 +0200

    Fixed Photo Route Bug

--- server/routes/photo.js
+++ server/routes/photo.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const path = require('path');
 const router = express.Router();
-const controller = require(path.join(__dirname, '../controllers/photos_controller.js'))
+const controller = require(path.join(__dirname, '../controllers/photos_controller'))
 
 
 router.get('/', (req, res) => {
