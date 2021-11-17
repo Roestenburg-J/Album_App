00128188f3eb28a079c6be27425a3063abcf2154
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-11-17 09:30:02 +0200

    Resotred createPhoto method, renamed photo_controller

---
+++ server/controllers/photo_controller.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const sql = require('mssql');
+const connection = require(path.join(__dirname, '../database/connection'));
+
+const getPhotos = async (req, res) => {
+    const pool = await connection.getConnection();
+    const result = await pool.request().query("SELECT * FROM PHOTO");
+    res.json(result.recordset)
+}
+
+const createPhoto = async (req, res) => {
+
+    const { datetime, filepath, format, location, userid, takenby } = req.body;
+
+    if (datetime == null || filepath == null || format == null || location == null || userid == null || takenby == null) {
+        return res.status(400).json({ msg: "Bad request, please fill in all fields" });
+    }
+
+    const pool = await connection.getConnection();
+    const result = await pool.request()
+        .input("datetime", sql.DateTime, Date())
+        .input("filepath", sql.Text, filepath)
+        .input("format", sql.Text, format)
+        .input("location", sql.Text, location)
+        .input("userid", sql.Int, userid)
+        .input("takenby", sql.Text, takenby)
+        // .input("datetime", sql.DateTime, Date())
+        // .input("filepath", sql.Text, "InputTest")
+        // .input("format", sql.Text, "TestFormat")
+        // .input("location", sql.Text, "TestLocation")
+        // .input("userid", sql.Int, "1")
+        // .input("takenby", sql.Text, "Test")
+        .query(
+            "INSERT INTO PHOTO (PHOTO_DATE_TIME, PHOTO_FILE_PATH, PHOTO_FORMAT, PHOTO_LOCATION, USER_ID, PHOTO_TAKEN_BY) VALUES (@datetime, @filepath, @format, @location, @userid, @takenby)"
+        );
+
+    res.status(200).json("new photo added!");
+
+}
+
+module.exports.getPhotos = getPhotos;
+module.exports.createPhoto = createPhoto;
--- server/routes/photo.js
+++ server/routes/photo.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const router = express.Router();
-const controller = require(path.join(__dirname, '../controllers/photos_controller'))
+const controller = require(path.join(__dirname, '../controllers/photo_controller'))
 
 router.get('/', (req, res) => {
 
