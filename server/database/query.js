ae67a6709ae9e039adda48441f0c04b1093cf0cb
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-11-17 14:44:49 +0200

    Added album Queries

--- server/database/query.js
+++ server/database/query.js
@@ -4,8 +4,13 @@ const query = {
     createPhoto: "INSERT INTO PHOTO (PHOTO_DATE_TIME, PHOTO_FILE_PATH, PHOTO_FORMAT, PHOTO_LOCATION, USER_ID, PHOTO_TAKEN_BY) VALUES (@datetime, @filepath, @format, @location, @userid, @takenby)",
     getPhotoByID: "SELECT * FROM PHOTO WHERE PHOTO_ID = @id",
     deletePhotoByID: "DELETE FROM PHOTO WHERE PHOTO_ID = @id",
-    updatePhotoByID: "UPDATE PHOTO SET PHOTO_DATE_TIME = @datetime, PHOTO_FILE_PATH = @filepath, PHOTO_FORMAT = @format, PHOTO_LOCATION = @location, USER_ID = @userid, PHOTO_TAKEN_BY = @takenby WHERE PHOTO_ID = @id"
+    updatePhotoByID: "UPDATE PHOTO SET PHOTO_DATE_TIME = @datetime, PHOTO_FILE_PATH = @filepath, PHOTO_FORMAT = @format, PHOTO_LOCATION = @location, USER_ID = @userid, PHOTO_TAKEN_BY = @takenby WHERE PHOTO_ID = @id",
+
     //Album
+    getAlbums: "SELECT * FROM ALBUM",
+    createAlbum: "INSERT INTO ALBUM (ALBUM_NAME, ALBUM_DESCRIPTION, ALBUM_DATE_CREATED, ALBUM_CREATED_BY) VALUES (@name, @description, @adatetime, @createdBy)",
+    getAlbumByID: "SELECT * FROM ALBUM WHERE ALBUM_ID = @id",
+    deleteAlbumByID: "DELETE FROM PHOTO WHERE PHOTO_ID = @id"
 }
 
 module.exports = query;
