8b67695ca2862decc70efe1c87b852b5ae823a44
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-11-18 16:35:43 +0200

    Added Missing UPDATE album Query

--- server/database/query.js
+++ server/database/query.js
@@ -8,14 +8,22 @@ const query = {
 
     //Album
     getAlbums: "SELECT * FROM ALBUM",
-    createAlbum: "INSERT INTO ALBUM (ALBUM_NAME, ALBUM_DESCRIPTION, ALBUM_DATE_CREATED, ALBUM_CREATED_BY) VALUES (@name, @description, @adatetime, @createdBy)",
+    createAlbum: "INSERT INTO ALBUM (ALBUM_NAME, ALBUM_DESCRIPTION, ALBUM_DATE_CREATED, USER_ID) VALUES (@name, @description, @adatetime, @userid)",
     getAlbumByID: "SELECT * FROM ALBUM WHERE ALBUM_ID = @id",
     deleteAlbumByID: "DELETE FROM ALBUM WHERE ALBUM_ID = @id",
+    updateAlbumByID: "UPDATE ALBUM SET ALBUM_NAME = @name, ALBUM_DESCRIPTION = @description, ALBUM_DATE_CREATED = @adatetime, USER_ID = @userid WHERE ALBUM_ID = @id",
 
     //PhotoTag
     getPhotoTags: "SELECT * FROM PHOTO_TAG WHERE PHOTO_ID = @id",
     createPhotoTag: "INSERT INTO PHOTO_TAG (PHOTO_ID, TAG) VALUES (@photoid, @tag )",
-    deletePhotoTag: "DELETE FROM PHOTO_TAG WHERE PHOTO_ID = @id AND TAG = @tag"
+    deletePhotoTag: "DELETE FROM PHOTO_TAG WHERE PHOTO_ID = @id AND TAG = @tag",
+
+    //PhotoAlbum
+    getAlbumPhotos: "SELECT PHOTO_ID FROM ALBUM_PHOTO WHERE ALBUM_ID = @id",
+    addPhotoToAlbum: "INSERT INTO ALBUM_PHOTO (ALBUM_ID, PHOTO_ID) VALUES (@albumid, @photoid)",
+    deletePhotoFromAlbum: "DELETE FROM ALBUM WHERE PHOTO_ID = @photoid AND ALBUM_ID = @albumid"
+
+
 }
 
 module.exports = query;
