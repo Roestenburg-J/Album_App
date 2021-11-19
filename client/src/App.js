7e78f03d7fcca6475620bf81f5aa25ea164b005c
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-11-19 21:30:36 +0200

    revamped fontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,21 @@
-import './App.css';
-import { useState } from 'react'
+import React, { useState } from "react";
+import Axios from "axios";
+import photos from "./mock_data.json";
+import "./public/css/App.css";
 
 
 function App() {
 
-  const [date, setDate] = useState("");
-  const [location, setLocation] = useState("")
-  const [takenby, setTakenBy] = useState("")
+  const [imageList, setImageList] = useState(photos.resources)
+  return (<div className="image-gird">
+    {imageList.map((image) => (
+      <img src={(image.url)} alt="" ></img>
+    ))
+    }
 
-  return (
-    <div className="App">
-
-      <div className="photo-meta">
-        <label>Date and Time:</label>
-        <input type="text" onChange={(event) => { setDate(event.target.value); }} />
-        <label>Location:</label>
-        <input type="text" onChange={(event) => { setLocation(event.target.value); }} />
-        <label>Taken By:</label>
-        <input type="text" onChange={(event) => { setTakenBy(event.target.value); }} />
-
-        <div className="photo">
-          <form action="/">
-            <label for="img">Select image:</label>
-            <input type="file" id="img" name="img" accept="image/*"></input>
-          </form>
-        </div>
-
-        <button>Upload Photo</button>
-      </div>
-    </div >
+  </div >
   );
+
 }
 
 export default App;
